d214444eba1fe15c36fd2effb3df0b6384fbf12e
Fournier De Lauriere  Camille <cfournier@ethz.ch>
2025-11-19 13:43:02 +0100

    add cheatsheet

--- 06_tidyDataGgplot/tidyDataGgplot.R
+++ 06_tidyDataGgplot/tidyDataGgplot.R
@@ -18,6 +18,24 @@ iris
 ggplot(data = , aes())+
   geom_...()
 
+ggplot(
+  iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species, shape = Species)
+)+
+  geom_point()+
+  geom_smooth(method= "lm")
+
+versicolorIris <- iris %>% filter(Species == "versicolor")
+
+ggplot(
+  versicolorIris
+)+
+  geom_point(aes(x = Sepal.Length, y = Sepal.Width))+
+  geom_smooth(aes(x = Sepal.Length, y = Sepal.Width),
+              method= "lm", color = "green")
+
+#+
+  # geom_smooth(method = "lm")
+
 # >> Are the length of the Petal & Sepal related to each other, in the iris dataset?
 
 # You can use the slides here, the tutorial linked here and in the beginning of the code
@@ -30,6 +48,14 @@ mtcars # is this a long-format dataset?
 
 # >> build the plot from the slides
 
+ggplot(data = mtcars) +
+  geom_point(mapping = aes(x = wt, y = mpg,
+                           colour = disp,
+                           size = hp))+
+  labs(x = "Weight", y = "Miles per Gallon", color = "Displacement", size = "Horse Power",
+       title = "Car efficiency based on weight, split by manual and automatic")+
+  facet_wrap(~am)+
+  theme_bw()
 
 
 ### using dplyr
@@ -65,8 +91,7 @@ irisModified <- iris %>%
   summarize(mean_ratioLength = mean(ratioPetalSepalLength, na.rm=T)) %>% 
   pull(mean_ratioLength) # pull extracts the values in a column and prints them
   
-
-# merge datasets together
+# merge datasets together (actually from base, but is more intuitive than the dplyr options I.M.O)
 # example, do not run
 dfMerged <- merge(df1, df2, by = "columnid", all.x = T, all.y = T) # this would be a full merge, what's the default behavior?
 
@@ -91,22 +116,87 @@ irisMerged2
 
 # make sure you understand the difference between irisMerged and irisMerged2
 
+# >> let's select a few variables of interest for your project, and visualize their distribution (single variable, 2 variables and their measures of association)
 
-#> use it all together, think about how to make the plots described in the slides
+theme_set(theme_minimal()) # set default theme for the session
 
-band_members
-band_instruments
+# load ESS data:
+ESS9_CH <- read.csv("ESSData/ESS9_CH.csv") # replace with relevant dataset for your project
 
-# create bandsInstrumentsMerged
+# select columns 
+ESS9_CH_proj <- ESS9_CH %>% 
+  select(prtclgch, trstprt, aesfdrk) #  prtclgch - Which party feel closer to, Switzerland --  trstprt - Trust in political parties --  aesfdrk - Feeling of safety of walking alone in local area after dark 
 
-# >> together, use geom_bar to create the plot in the slides
+ESS9_CH_proj %>% plot()
 
-# >> again, another plot, this time do it yourself
+# rename columns
+ESS9_CH_proj <- ESS9_CH_proj %>% 
+  rename(party = prtclgch,
+         trustPol = trstprt,
+         safety = aesfdrk)
 
-# using merge(),
-# filter(),
-# ggplot()+
-#   geom_bar()
+# visualize one variable (and removing non-applicable values)
+ESS9_CH_proj %>% 
+  ggplot(aes(x = party))+
+  geom_bar()
+
+ESS9_CH_proj %>% 
+  filter(party < 17) %>%
+  ggplot(aes(x = party))+
+  geom_bar()
+
+ESS9_CH_proj %>% 
+  ggplot(aes(x = trustPol))+
+  geom_bar()
+
+ESS9_CH_proj %>% 
+  filter(trustPol <= 10) %>% 
+  ggplot(aes(x = trustPol))+
+  geom_bar()+
+  labs(x = "Trust in political parties",
+       y = "Number of respondents")
+
+ESS9_CH_proj %>% 
+  ggplot(aes(x = safety))+
+  geom_bar()
+
+# associations between variables
+ESS9_CH_proj %>% 
+  ggplot(aes(x = safety, y = trustPol, group = safety))+
+  geom_boxplot()
+
+# here, could decide to create a new 'filtered' dataframe, or to use the pipes every time: e.g.
+
+ESS9_CH_filt <- ESS9_CH_proj %>% 
+  filter(trustPol <= 10) %>% 
+  filter(party < 17) %>% 
+  filter(safety <= 4)
+
+ESS9_CH_filt %>% 
+  ggplot(aes(x = safety, y = trustPol, group = safety))+
+  geom_boxplot()
+
+ESS9_CH_filt %>% 
+  group_by(safety) %>% 
+  summarize(avgTrustPol = mean(trustPol, na.rm =T),
+            sampleSize = n())
+
+cor(ESS9_CH_filt$trustPol, ESS9_CH_filt$safety)
+cor.test(ESS9_CH_filt$trustPol, ESS9_CH_filt$safety)
+
+# lots of other functions in tidyverse, for example:
+# - to transform a wide dataset into long: pivot_longer() from tidyr
+# - to order a dataset based on a variable: arrange() 
+# - to select the first n rows: slice()
+# - to extract a vector from the column (instead of using $column, can use `df %>% pull(column)` )
+
+lm(trustPol~safety, data = ESS9_CH_filt) %>% summary()
+
+lm1 <- lm(trustPol~safety, data = ESS9_CH_filt)
+plot(lm1) # (need to press enter multiple times in the console to visualize different plots)
+
+resid(lm1)
+
 
 
 
